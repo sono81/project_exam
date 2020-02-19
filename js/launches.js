095a61b3b3a30d80543c8529bc80d78aef604e4c
Sondre N <49595556+sono81@users.noreply.github.com>
2020-02-19 13:51:54 +0100

    Add launches and missions.js. Small update to .css

---
+++ js/launches.js
@@ -0,0 +1,96 @@
+var launchArray = [];
+var selection = document.querySelector('#selection');
+var content = document.querySelector('#content');
+
+fetch('https://api.spacexdata.com/v3/launches/')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (json) {
+        populateSelect(json);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+    function populateSelect(launchObj) {
+        launchArray = launchObj;
+        launchArray.forEach(function(launch) {
+            selection.innerHTML += `
+            <option value="${launch.mission_name}">${launch.mission_name}</option>
+            `
+        });
+        console.log(launchObj);
+    };
+
+    selection.addEventListener('change', filterLaunches);
+    function filterLaunches() {
+        var selectedValue = selection.options[selection.selectedIndex].text;
+        var lowerCaseSearch = selectedValue.toLowerCase();
+        var filteredLaunches = launchArray.filter(function(launch) {
+            var lowerCase = launch.mission_name.toLowerCase();
+            if (lowerCase.startsWith(lowerCaseSearch)) {
+                return true
+            }
+        });
+
+        content.innerHTML = "";
+
+        filteredLaunches.forEach(function(launch) {
+
+            var success = `${launch.launch_success}`
+            var date = `${launch.launch_date_utc}`;
+            date = date.slice(0,10);
+
+            if (success === 'true') {
+                success = 'Yes'
+            } else {
+                success = 'No'
+            }
+
+
+            content.innerHTML += `
+            <table>
+                <tr>
+                    <td>Name:</td>
+                    <td>${launch.mission_name}</td>
+                </tr>
+                <tr>
+                    <td>Id:</td>
+                    <td>${launch.mission_id}</td>
+                </tr>
+                <tr>
+                    <td>Rocket:</td>
+                    <td>${launch.rocket.rocket_name}</td>
+                </tr>
+                <tr>
+                    <td>Launch Date (UTC):</td>
+                    <td>${date}</td>
+                </tr>
+                <tr>
+                    <td>Launch Site:</td>
+                    <td>${launch.launch_site.site_name}</td>
+                </tr>
+                <tr>
+                    <td>Launch Success:</td>
+                    <td>${success}</td>
+                </tr>
+                <tr>
+                    <td>Article:</td>
+                    <td><a href="${launch.links.article_link}">Article on ${launch.mission_name}</a></td>
+                </tr>
+                <tr>
+                    <td>Wikipedia:</td>
+                    <td><a href="${launch.links.wikipedia}">${launch.mission_name}'s wikipedia page</a></td>
+                </tr>
+                <tr>
+                    <td>Details:</td>
+                    <td>${launch.details}</td>
+                </tr>
+
+            </table>
+
+            <img src="${launch.links.mission_patch_small}" alt="mission patch" class="patch">
+            `
+        });
+    };
---
+++ js/missions.js
@@ -0,0 +1,77 @@
+var missionArray = [];
+var selection = document.querySelector('#selection');
+var content = document.querySelector('#content');
+
+fetch('https://api.spacexdata.com/v3/missions/')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (json) {
+        populateSelect(json);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+    function populateSelect(missionObj) {
+        missionArray = missionObj;
+        missionArray.forEach(function(mission) {
+            selection.innerHTML += `
+            <option value="${mission.mission_name}">${mission.mission_name}</option>
+            `
+        });
+        console.log(missionObj);
+    };
+
+    selection.addEventListener('change', filterMissions);
+    function filterMissions() {
+        var selectedValue = selection.options[selection.selectedIndex].text;
+        var lowerCaseSearch = selectedValue.toLowerCase();
+        var filteredMissions = missionArray.filter(function(mission) {
+            var lowerCase = mission.mission_name.toLowerCase();
+            if (lowerCase.startsWith(lowerCaseSearch)) {
+                return true
+            }
+        });
+
+        content.innerHTML = "";
+
+        filteredMissions.forEach(function(mission) {
+
+            var str = `${mission.payload_ids}`.split(',').join('<br>');
+
+            content.innerHTML += `
+            <table>
+                <tr>
+                    <td>Name:</td>
+                    <td>${mission.mission_name}</td>
+                </tr>
+                <tr>
+                    <td>Id:</td>
+                    <td>${mission.mission_id}</td>
+                </tr>
+                <tr>
+                    <td>Manufacturers:</td>
+                    <td>${mission.manufacturers}</td>
+                </tr>
+                <tr>
+                    <td>Payload id's:</td>
+                    <td>${str}</td>
+                </tr>
+                <tr>
+                    <td>-----</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Wikipedia:</td>
+                    <td><a href="${mission.wikipedia}">${mission.mission_name}'s wikipedia page</a></td>
+                </tr>
+                <tr>
+                    <td>Description:</td>
+                    <td>${mission.description}</td>
+                </tr>
+
+            </table>
+            `
+        });
+    };
